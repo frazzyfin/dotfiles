23021792e7f676afcac0f1be3e435a7a96a8f16b
Fraser Finlay <fraserfinlay@gmail.com>
2023-03-07 13:56:56 +0000

    Added primeagen keybindings

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -15,3 +15,18 @@ vim.opt.shiftround = true
 vim.opt.expandtab = true
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- when lines are highlighted move the whole block up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- keep cursor in the middle when jumping up and down
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+-- keep buffer when pasting over existing text
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- yank to system clipboard 
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
